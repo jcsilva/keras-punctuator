97b74179420e5389a31f066eb6ba2a804f2d93ac
Eduardo Silva <zedudu@gmail.com>
2016-03-16 21:31:40 -0300

    Remove pauses
    I'll not use "pauses" at this moment. In the future, if necessary,
    I may use this information.

--- src/myconverter.py
+++ src/myconverter.py
@@ -6,24 +6,17 @@ import utils
 import h5py
 import os
 
-def convert_files(file_paths, vocabulary, punctuations, use_pauses, output_path):
+def convert_files(file_paths, vocabulary, punctuations, output_path):
     inputs = []
     outputs = []
     punctuation = " "
-    pause = 0.
     
-    if use_pauses:
-        pauses = []
-
     for file_path in file_paths:
         with open(file_path, 'r') as corpus:
             for line in corpus:
                 for token in line.split():
                     if token in punctuations:
                         punctuation = token
-                        continue
-                    elif token.startswith("<sil="):
-                        pause = float(token.replace("<sil=","").replace(">",""))
                         continue
                     else:
                         array = np.zeros(shape=(1, len(vocabulary)), dtype=np.int8)
@@ -32,10 +25,7 @@ def convert_files(file_paths, vocabulary, punctuations, use_pauses, output_path)
                         array = np.zeros(shape=(1, len(punctuations)), dtype=np.int8)
                         array[0,utils.punctuation_index(punctuations, punctuation)] = 1
                         outputs.append(array)
-                        if use_pauses:
-                            pauses.append(pause)
                         punctuation = " "
-                        pause = 0.
 
                 array = np.zeros(shape=(1, len(vocabulary)), dtype=np.int8)
                 array[0,utils.input_word_index(vocabulary, "<END>")] = 1
@@ -45,30 +35,21 @@ def convert_files(file_paths, vocabulary, punctuations, use_pauses, output_path)
                 array[0,utils.punctuation_index(punctuations, punctuation)] = 1
                 outputs.append(array)
 
-#    if use_pauses:
-#        pauses.append(pause)
-
     assert len(inputs) == len(outputs)
 
     inputs = np.array(inputs, dtype=np.int8).reshape((len(inputs), 1, len(vocabulary)))
     outputs = np.array(outputs, dtype=np.int16).reshape((len(inputs), len(punctuations)))
-##    if use_pauses:
-##       pauses = np.array(pauses, dtype=np.float32) #[:batch_size*num_batches].reshape((batch_size, num_batches)).T
-
-    total_size = len(inputs) #batch_size*num_batches
 
     f = h5py.File(output_path + '.h5', "w")
     dset = f.create_dataset('inputs', data=inputs, dtype='i8')
     dset = f.create_dataset('outputs',data=outputs, dtype='i8')
 
     data = {"vocabulary": vocabulary, "punctuations": punctuations, 
-           "total_size": total_size}
+           "total_size": len(inputs)}
     
-##    if use_pauses:
-##        data["pauses"] = pauses
-
     with open(output_path + '.pkl', 'wb') as output_file:
         cPickle.dump(data, output_file, protocol=cPickle.HIGHEST_PROTOCOL)
+
 
 
 PHASE1_TRAIN_PATH = "../data/train1"
@@ -78,15 +59,12 @@ VOCABULARY_FILE = "../raw_data/vocab"
 TRAIN_DATA = "../raw_data/train.txt"
 DEV_DATA = "../raw_data/dev.txt"
 
-
 if not os.path.exists("../data"):
   os.makedirs("../data")
 
-print "Converting data...\n"
+print("Converting data...")
 
 vocabulary = utils.load_vocabulary(VOCABULARY_FILE)
 
-convert_files([TRAIN_DATA], vocabulary, PUNCTUATIONS,  False, PHASE1_TRAIN_PATH)
-
-convert_files([DEV_DATA], vocabulary, PUNCTUATIONS, False, PHASE1_DEV_PATH)
-
+convert_files([TRAIN_DATA], vocabulary, PUNCTUATIONS, PHASE1_TRAIN_PATH)
+convert_files([DEV_DATA], vocabulary, PUNCTUATIONS, PHASE1_DEV_PATH)
