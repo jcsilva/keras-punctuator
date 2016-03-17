6fe1efae77ea1c2c42fe9b2f8ccd93d43c096969
Eduardo Silva <zedudu@gmail.com>
2016-03-16 21:36:46 -0300

    Trainer script cleaner

--- src/keras_punctuator.py
+++ src/keras_punctuator.py
@@ -11,29 +11,29 @@ from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 from keras.callbacks import EarlyStopping
 from keras.utils.io_utils import HDF5Matrix
-
+from sklearn.metrics import classification_report
 
 data = np.load('../data/train1.pkl')
-total_size = data['total_size']
-vocabulary = data['vocabulary']
+total_size  = data['total_size']
+vocabulary  = data['vocabulary']
 punctuation = data['punctuations']
 
-input_data_size = len(vocabulary)
+input_data_size  = len(vocabulary)
 output_data_size = len(punctuation)
+train_amount     = 0.85
 
 # Adapted from https://github.com/fchollet/keras/issues/68#issuecomment-94881051
 def load_data(datapath, train_start, n_training_examples, test_start, n_test_examples):
-    X_train = HDF5Matrix(datapath, 'inputs', train_start, train_start+n_training_examples)
+    X_train = HDF5Matrix(datapath, 'inputs',  train_start, train_start+n_training_examples)
     y_train = HDF5Matrix(datapath, 'outputs', train_start, train_start+n_training_examples)
-    X_test = HDF5Matrix(datapath, 'inputs', test_start, test_start+n_test_examples)
-    y_test = HDF5Matrix(datapath, 'outputs', test_start, test_start+n_test_examples)
+    X_test  = HDF5Matrix(datapath, 'inputs',  test_start,  test_start+n_test_examples)
+    y_test  = HDF5Matrix(datapath, 'outputs', test_start,  test_start+n_test_examples)
     return X_train, y_train, X_test, y_test
 
 X_train, y_train, X_test, y_test = load_data('../data/train1.h5', 
-                                   0, int(math.floor(0.85*total_size)), 
-                                   int(math.ceil(0.85*total_size)), 
-                                   int(total_size-math.ceil(0.85*total_size)))
-
+                                   0, int(math.floor(train_amount*total_size)), 
+                                   int(math.ceil(train_amount*total_size)), 
+                                   int(total_size-math.ceil(train_amount*total_size)))
 
 print('Build model...')
 
@@ -48,9 +48,9 @@ model.add(Dense(output_data_size, init="uniform"))
 model.add(Activation("softmax"))
 model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
 
-
 earlyStopping=EarlyStopping(monitor='val_loss', patience=2, verbose=0, mode='auto')
 
+# Train 
 hist = model.fit(X_train, y_train, 
                  batch_size=200, 
                  nb_epoch=150, 
@@ -59,38 +59,19 @@ hist = model.fit(X_train, y_train,
                  callbacks=[earlyStopping], 
                  shuffle='batch')
 
-#score, acc = model.evaluate(X_test, y_test,
-#                            batch_size=16,
-#                            show_accuracy=True)
-
-#print('Test score:', score)
-#print('Test accuracy:', acc)
-#print(hist.history)
-
-
 # Predictions
 y_pred = model.predict(X_test)
 
-
-from sklearn.metrics import classification_report
-
+# Classification report
 y_gen = np.argmax(y_pred, axis=1)
 y_ref = list()
 for i in y_test:
   y_ref.append(np.argmax(i))
 
-print('y_ref argmax: ' + str(np.argmax(y_ref)) + ' y_gen argmax: ' + str(np.argmax(y_gen)))
-
 print(classification_report(y_ref, y_gen))
 
-
-
-
-
-
-# Save models
+# Save model and weights
 json_string = model.to_json()
 with open('model_architecture.json', 'w') as output_file:
   output_file.write(json_string)
 model.save_weights('model_weights.h5', overwrite=True)
-
